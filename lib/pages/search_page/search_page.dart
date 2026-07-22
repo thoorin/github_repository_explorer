92be529e85cc5883758ecd68ca749d0bf2f03f64
marekskreko <marek.skreko@nam.cz>
2026-07-22 16:44:25 +0200

    feat: add basic layout to SearchPage

--- lib/pages/search_page/search_page.dart
+++ lib/pages/search_page/search_page.dart
@@ -11,10 +11,36 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(),
-    body: const Center(
+    body: SafeArea(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[Text('You have pushed the button this many times:')],
+        children: [
+          const Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[SearchBar()],
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(color: Colors.blue),
+                  child: const Icon(Icons.search, size: 100),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(color: Colors.green),
+                  child: const Icon(Icons.favorite_border, size: 100),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     ),
   );
