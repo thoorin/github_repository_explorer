8deff1d407bddac3cc65ff6075667892617a7661
marekskreko <marek.skreko@nam.cz>
2026-07-22 19:22:34 +0200

    tests: correct test tags

--- test/search_repositories_test.dart
+++ test/search_repositories_test.dart
@@ -3,13 +3,13 @@ import 'package:github_repository_explorer/api/github_api.dart' as github_api;
 import 'package:github_repository_explorer/api/github_responses.dart';
 
 void main() {
-  test('', () async {
+  test('Invalid', () async {
     final response = await github_api.searchRepositories('');
     expect(response.statusCode, SearchRepositoryStatusCode.validationFailed);
     expect(response.repositories, equals([]));
   });
 
-  test('2', () async {
+  test('Valid but without result', () async {
     final response = await github_api.searchRepositories(
       '3241768914379681348769123786912347689148794168179462',
     );
@@ -17,9 +17,8 @@ void main() {
     expect(response.repositories, equals([]));
   });
 
-  test('3', () async {
+  test('Valid', () async {
     final response = await github_api.searchRepositories('github');
     expect(response.statusCode, SearchRepositoryStatusCode.ok);
-    expect(response.repositories, equals([]));
   });
 }
